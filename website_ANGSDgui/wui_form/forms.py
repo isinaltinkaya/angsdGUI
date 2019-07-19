bba0f1f528bf09ca7a3d5854408d7762d7b3d552
isinaltinkaya <isinaltinkaya@gmail.com>
2019-07-19 15:23:34 +0100

    add bootstrap and regex

--- website_ANGSDgui/wui_form/forms.py
+++ website_ANGSDgui/wui_form/forms.py
@@ -1,7 +1,26 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
+from django.core import validators
 from django import forms
 from . import choices
 
 class UI_Form(forms.Form):
-    pipeline_name = forms.CharField(required=False, label="Name of pipeline")
+    pipeline_name = forms.CharField(required=False,
+            label="Name of pipeline",
+            validators=[
+                validators.RegexValidator(regex='^[a-zA-Z0-9]*$', message='Please enter a valid pipeline name.'),
+                ])
     input_filename = forms.CharField(label="Input file name")
     input_filetype = forms.ChoiceField(choices=choices.fileTypes,label="Input file type")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper
+        self.helper.form_method = 'post' # use post method
+        self.helper.layout = Layout(
+                'pipeline_name',
+                'input_filename',
+                'input_filetype',
+                Submit('submit', 'Submit', css_class='btn-success')
+        )
--- website_ANGSDgui/wui_form/views.py
+++ website_ANGSDgui/wui_form/views.py
@@ -4,14 +4,19 @@ from .forms import UI_Form
 from . import pipeline_generator as gen
 
 def get_input_details(request):
+    
     if request.method == 'POST':
         form = UI_Form(request.POST)
         if form.is_valid():
+            
             pipeline_name = form.cleaned_data['pipeline_name']
             input_filename = form.cleaned_data['input_filename']
             input_filetype = form.cleaned_data['input_filetype']
             print(pipeline_name,input_filename,input_filetype)
             gen.touch_outFile(pipeline_name)
             gen.get_input(input_filename,input_filetype)
-    form = UI_Form()
+    
+    else:
+        form = UI_Form()
+    
     return render(request, 'form.html', {'form': form})
