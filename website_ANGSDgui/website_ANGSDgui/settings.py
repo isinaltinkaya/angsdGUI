ac38cbd9529c1bcc4698375ed112777b70249f3f
isinaltinkaya <isinaltinkaya@gmail.com>
2019-08-05 12:15:38 +0100

    js in progress

--- website_ANGSDgui/website_ANGSDgui/settings.py
+++ website_ANGSDgui/website_ANGSDgui/settings.py
@@ -125,3 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+# Put strings here, like "/home/html/static" or "C:/www/django/static".
+# Always use forward slashes, even on Windows.
+# Don't forget to use absolute paths, not relative paths.
+#'/Users/a9austin/Development/sites/AlphaSocks/src/staticfiles'
+
+)
