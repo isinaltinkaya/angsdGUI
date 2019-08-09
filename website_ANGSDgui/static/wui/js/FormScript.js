3314b4847089db0bd72ea4509e1bfa1a740b7232
isinaltinkaya <isinaltinkaya@gmail.com>
2019-08-09 16:26:13 +0100

    collectstatic FormScript.js from previous commit

--- website_ANGSDgui/static/wui/js/FormScript.js
+++ website_ANGSDgui/static/wui/js/FormScript.js
@@ -1,20 +1,27 @@
-// define empty dictionary
-var dict = {};
+//// notes for next week >>
+// create a general updateCode function
+// add onkeyup to every input
+// calculate output and update in every single onkeyup event
+// define var a, add stuff to it
+// if b; add a for non-required b's
+//
+// how to handle:
+// multiple users using ui in the same time
+
 
 // add functions to the dictionary
-dict.softwarePath = function(){
-    element = document.getElementById("softwarePath")
-//    if (element.childNodes
-    input = element.value;
-    document.getElementById("code").innerHTML += input;
-}
-
-dict.infileName = function(){
-    input = document.getElementById("infileName").value;
-    document.getElementById("code").innerHTML += input;
-}
 
 // update code onkeyup
-function updateCode(id){
-    dict[id]();
+class updateCode(userId){
+    softwarePath(){
+        element = document.getElementById("softwarePath")
+        input = element.value;
+        document.getElementById("code").innerHTML += input;
+    }
+
+    infileName(){
+        input = document.getElementById("infileName").value;
+        document.getElementById("code").innerHTML += input;
+    }
 }
+
