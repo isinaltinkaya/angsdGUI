9386ac2f35701ab4731f0d8916bc7a7d690a8f2d
isinaltinkaya <isinaltinkaya@gmail.com>
2020-02-25 17:56:18 +0300

    call software from JSON

--- website_ANGSDgui/wui/static/wui/js/FormScript.js
+++ website_ANGSDgui/wui/static/wui/js/FormScript.js
@@ -9,6 +9,11 @@ var PipelineName = document.getElementById("pipelineName");
 var YourPipelineName = document.getElementById("yourPipelineName");
 var Analysis = document.getElementById("analysis");  
 
+// declare global variables
+var AnalysisName;
+var Software;
+var Json;
+
 // read json file using jquery
 var getJSON = $.ajax({
     url: '/static/wui/js/Analyses.json',
@@ -16,8 +21,9 @@ var getJSON = $.ajax({
     dataType: 'json',
     dataContent: 'application/json',
     data: {get_param: 'value'},
-    //success: function (data) {
-        //console.log("success")  }
+    success: function (data) {
+        Json = getJSON.responseJSON;
+    }
 });
 
 // define selection attributes
@@ -176,7 +182,7 @@ var createSelect = function(Select, SelectId, OptionList, Required, onChange){
 
 
 // add item to a specific div
-var addFormItem = function(DivId, ObjectPop, ObjectLabel, ObjectName, Generation){
+var addFormItem = function(DivId, ObjectName, ObjectLabel, ObjectPop, Generation){
     var Div = document.getElementById(DivId);
     var Div1 = document.createElement('div');
     Div1.classList.add("formItem");
@@ -192,81 +198,6 @@ var addFormItem = function(DivId, ObjectPop, ObjectLabel, ObjectName, Generation
     Div.appendChild(DocumentFragment);
 }
 
-
-// get software path if exists
-var getSoftwarePath = function(Software){
-
-    var SoftwarePath = document.getElementById(Software);
-
-    if(SoftwarePath){
-
-        if(SoftwarePath.value){
-            return SoftwarePath.value;
-        }
-        return Software;
-    }
-    return Software;
-}
-
-
-// call required software
-var callSoftware = function(Software){
-    
-    if (Software == "realSFS"){
-
-        var realSFS = document.getElementById("realSFS");
-        if (realSFS){
-            return getSoftwarePath(Software); 
-        }
-        /*
-        if realSFS input does not exist
-        create realSFS path input
-        */
-        var realSFS = document.createElement('input');
-        createInput(realSFS, "realSFS");
-        var realSFSLabel = document.createElement('label');
-        createLabel(realSFSLabel, realSFS.id, " Path to realSFS ");
-        var realSFSPop = document.createElement('a');
-        createPopover(
-                realSFSPop, 
-                "Path to realSFS Software", 
-                `Path to realSFS in your file system. If empty, realSFS will be called as <code>realSFS</code>.<br />
-                For Linux systems, it can be found with command <code>which realSFS</code>.<br />
-                e.g. <code>/usr/bin/angsd/misc/realSFS</code>
-                `
-        );
-        addFormItem("step-2-div", realSFSPop, realSFSLabel, realSFS, "G2");
-
-        // enable new popovers
-        enableNewPops();
-
-    } else if (Software == "angsd"){
-        
-        var angsd = document.getElementById("angsd");
-        if (realSFS){
-            return getSoftwarePath(Software);
-        }
-
-        var angsd = document.createElement('input');
-        createInput(angsd, "angsd");
-        var angsdLabel = document.createElement('label');
-        createLabel(angsdLabel, angsd.id, " Path to angsd ");
-        var angsdPop = document.createElement('a');
-        createPopover(
-                angsdPop,
-                "Path to angsd Software", 
-                `Path to angsd in your file system. If empty, angsd will be called as <code>angsd</code>.<br />
-                For Linux systems, it can be found with command <code>which angsd</code>.<br />
-                e.g. <code>/usr/bin/angsd/angsd</code><br />
-                `
-        );
-        addFormItem("step-2-div", angsdPop, angsdLabel, angsd, "G2");
-
-        // enable new popovers
-        enableNewPops();
-
-    }
-}
 
 
 var nextStep = function(Step){
@@ -406,7 +337,11 @@ var clearGen = function(Generation){
 
 // InfileType onchange
 var ocInfileType = function(){
-    
+
+    // change analysis name according to infile type
+    AnalysisName = Analysis.options[Analysis.selectedIndex].value;
+
+
     // clear generation from previous choice
     clearGen("G2");
 
@@ -414,23 +349,18 @@ var ocInfileType = function(){
     rmSubSteps();
 
     var InfileType = document.getElementById("infileType");
-
+    
     // generate the new generation
     // if element exists
     if (InfileType){
 
-        if (InfileType.value){
+        if (InfileType.options[InfileType.selectedIndex].text){
 
-            var type = InfileType.value;
+            var AnalysisInfile = InfileType.options[InfileType.selectedIndex].text;
+            Software = Json[AnalysisName].Software[AnalysisInfile];
 
-            // special cases require functions
-            if (type == "saf"){
-                dorealSFS();
-            }
-            if (type == "-bam"){
-                callSoftware("angsd");
-                addBamOpt();
-            }
+            addSoftwarePath(Software)   ;
+
         }
     }
 
@@ -441,85 +371,85 @@ var ocInfileType = function(){
 
 
 // get infile type to modify pipeline
-var getInfileType = function(){
+//var getInfileType = function(){
 
-    var InfileType = document.getElementById("infileType");
+    //var InfileType = document.getElementById("infileType");
 
-    // if element exists
-    if (InfileType){
+    //// if element exists
+    //if (InfileType){
 
-        if (InfileType.value){
+        //if (InfileType.value){
 
-            var type = InfileType.value;
+            //var type = InfileType.value;
 
-            // special cases require functions
-            if (type == "saf"){
-                return getSoftwarePath("realSFS");
-            }
-            if (type == "-bam"){
-                return getSoftwarePath("angsd") + " " + InfileType.value;
-            }
-            return "";
-        }
-        return "";
-    }
-    return "";
-}
+            //// special cases require functions
+            //if (type == "saf"){
+                //return getSoftwarePath("realSFS");
+            //}
+            //if (type == "-bam"){
+                //return getSoftwarePath("angsd") + " " + InfileType.value;
+            //}
+            //return "";
+        //}
+        //return "";
+    //}
+    //return "";
+//}
 
 
-var dorealSFS = function(){
+//var dorealSFS = function(){
     
-    callSoftware("realSFS");
+    //callSoftware("realSFS");
 
-    // check region input
-    if (document.getElementById("realSFS_Region")){
-        return; 
-    }
+    //// check region input
+    //if (document.getElementById("realSFS_Region")){
+        //return; 
+    //}
 
-    // create region input
-    var realSFS_Region = document.createElement('input');
-    createInput(realSFS_Region, "realSFS_Region");
-    var realSFS_RegionLabel = document.createElement('label');
-    createLabel(realSFS_RegionLabel, realSFS_Region.id, " Region ");
-    var realSFS_RegionPop = document.createElement('a');
-    createPopover(
-            realSFS_RegionPop, 
-            "Define a region", 
-            `Do SFS with a chromosome or region.<br /> 
-            e.g. <code>chr22</code> for Chromosome 22<br />
-            e.g. <code>chr22:100000-2000000</code> for region 100000-2000000 in Chromosome 22.
-            `
-    );
-    addFormItem("step-2-div", realSFS_RegionPop, realSFS_RegionLabel, realSFS_Region, "G2");
-    FunctionList.push(getValue.bind(null, "-r", "realSFS_Region"));
-
-
-    // check nSites input
-    if (document.getElementById("realSFS_nSites")){
-        return; 
-    }
-
-    // create number of sites input
-    var realSFS_nSites = document.createElement('input');
-    createInput(realSFS_nSites, "realSFS_nSites");
-    var realSFS_nSitesLabel = document.createElement('label');
-    createLabel(realSFS_nSitesLabel, realSFS_nSites.id, " Number of sites ");
-    var realSFS_nSitesPop = document.createElement('a');
-    createPopover(
-            realSFS_nSitesPop, 
-            "Number of sites", 
-            `Limit SFS to a fixed number of sites. <br /> 
-             <code>-nSites</code> is used for choosing a max number of sites that should be used for the optimization. Using more sites will give you more reliable estimates. If you dont specify anything it will try to load all sites into memory. 
-            `
-    );
-    addFormItem("step-2-div", realSFS_nSitesPop, realSFS_nSitesLabel, realSFS_nSites, "G2");
-    FunctionList.push(getValue.bind(null, "-nSites", "realSFS_nSites"));
+    //// create region input
+    //var realSFS_Region = document.createElement('input');
+    //createInput(realSFS_Region, "realSFS_Region");
+    //var realSFS_RegionLabel = document.createElement('label');
+    //createLabel(realSFS_RegionLabel, realSFS_Region.id, " Region ");
+    //var realSFS_RegionPop = document.createElement('a');
+    //createPopover(
+            //realSFS_RegionPop, 
+            //"Define a region", 
+            //`Do SFS with a chromosome or region.<br /> 
+            //e.g. <code>chr22</code> for Chromosome 22<br />
+            //e.g. <code>chr22:100000-2000000</code> for region 100000-2000000 in Chromosome 22.
+            //`
+    //);
+    //addFormItem("step-2-div", realSFS_RegionPop, realSFS_RegionLabel, realSFS_Region, "G2");
+    //FunctionList.push(getValue.bind(null, "-r", "realSFS_Region"));
 
 
-    // enable new popovers
-    enableNewPops();
+    //// check nSites input
+    //if (document.getElementById("realSFS_nSites")){
+        //return; 
+    //}
 
-}
+    //// create number of sites input
+    //var realSFS_nSites = document.createElement('input');
+    //createInput(realSFS_nSites, "realSFS_nSites");
+    //var realSFS_nSitesLabel = document.createElement('label');
+    //createLabel(realSFS_nSitesLabel, realSFS_nSites.id, " Number of sites ");
+    //var realSFS_nSitesPop = document.createElement('a');
+    //createPopover(
+            //realSFS_nSitesPop, 
+            //"Number of sites", 
+            //`Limit SFS to a fixed number of sites. <br /> 
+             //<code>-nSites</code> is used for choosing a max number of sites that should be used for the optimization. Using more sites will give you more reliable estimates. If you dont specify anything it will try to load all sites into memory. 
+            //`
+    //);
+    //addFormItem("step-2-div", realSFS_nSitesPop, realSFS_nSitesLabel, realSFS_nSites, "G2");
+    //FunctionList.push(getValue.bind(null, "-nSites", "realSFS_nSites"));
+
+
+    //// enable new popovers
+    //enableNewPops();
+
+//}
 
 
 var getValue = function(param, id){
@@ -539,6 +469,62 @@ var getValue = function(param, id){
 }
 
 
+// get software path if exists
+var getSoftwarePath = function(Software){
+
+    var SoftwarePath = document.getElementById(Software);
+
+    // if element exists
+    if(SoftwarePath){
+
+        if(SoftwarePath.value){
+
+            // return what user wrote
+            return SoftwarePath.value;
+
+        }
+
+        return Json.Softwares[Software].Call;
+    }
+    return;
+}
+
+
+                //note
+                //return getSoftwarePath("angsd") + " " + InfileType.value;
+
+var addSoftwarePath = function(Software){
+    
+    //var existsSoft = document.getElementById("realSFS");
+    //if (realSFS){
+        //return getSoftwarePath(Software); 
+    //}
+    /*
+    if realSFS input does not exist
+    create realSFS path input
+    */
+    var SoftwareInfo = Json.Softwares[Software]
+    var Soft = document.createElement('input');
+    createInput(Soft, AnalysisName);
+    var SoftLabel = document.createElement('label');
+    createLabel(SoftLabel, SoftLabel.id, SoftwareInfo.Label);
+    var SoftPop = document.createElement('a');
+    createPopover(
+        SoftPop, 
+        SoftwareInfo.PopTitle, 
+        SoftwareInfo.PopExp
+    );
+    addFormItem("step-2-div", Soft, SoftLabel, SoftPop, "G2");
+
+    //FunctionList.push(getSoftwarePath(Software));
+
+    // enable new popovers
+    enableNewPops();
+
+}
+
+
+
 // define initial list of functions
 var FunctionList = [];
 
@@ -546,8 +532,7 @@ var FunctionList = [];
 // write step 2 according to analysis selection
 var getAnalysis = function(){
 
-    var Json = getJSON.responseJSON;
-    var AnalysisName = Analysis.options[Analysis.selectedIndex].value;
+    AnalysisName = Analysis.options[Analysis.selectedIndex].value;
 
     // remove previously generated substeps
     rmSubSteps();
@@ -557,90 +542,16 @@ var getAnalysis = function(){
 
     console.log("here1")
 
+    console.log(Analysis.options[Analysis.selectedIndex].value)
+
     // if analysis selected
     if (Analysis.options[Analysis.selectedIndex].value) {
 
-        // JsonAnalysis is the analysis id in JSON file
-        for (var JsonAnalysis in Json) {
-            
-            console.log("here2")
+        console.log("success")
 
-            if (AnalysisName == JsonAnalysis){
-
-                console.log("success")
-
-                addStep("step-2");
-
-                var InfileTypes = Json[JsonAnalysis].Infiles;
-
-                // create input file type select
-                var InfileType = document.createElement('select');  
-                createSelect(InfileType, "infileType", InfileTypes, "Required", "ocInfileType();");
-                var InfileTypeLabel = document.createElement('label');
-                createLabel(InfileTypeLabel, InfileType.id, " Input file type ", "Required");
-                var InfileTypePop = document.createElement('a');
-                createPopover(
-                    InfileTypePop, 
-                    "Input File Type", 
-                    `File type of input file. <br />
-                    For more information, see <a href="http://www.popgen.dk/angsd/index.php/Input" target="_blank">angsd wiki input page</a>.
-                    `
-                );
-                addFormItem("step-2-div", InfileTypePop, InfileTypeLabel, InfileType);
-                FunctionList.push(getInfileType);
-
-                // create input file name input
-                var InfileName = document.createElement('input');
-                createInput(InfileName, "infileName", "Required");
-                var InfileNameLabel = document.createElement('label');
-                createLabel(InfileNameLabel, InfileName.id, " Input file name ", "Required");
-                var InfileNamePop = document.createElement('a');
-                createPopover(
-                    InfileNamePop, 
-                    "Input File Name", 
-                    `Name of the input file to be used in the analysis. Including extensions if any.
-                    Provide path to input file if you are not going to run the code in the same directory as the input file.
-                    e.g. <code>/home/myuser/project/myfile.bamlist</code>`
-                );
-                addFormItem("step-2-div", InfileNamePop, InfileNameLabel, InfileName);
-                FunctionList.push(getValue.bind(null, "", "infileName"));
-
-                // enable new popovers
-                enableNewPops();
-
-                // add analysis name to pipeline header
-                CodeHeader = "#!bin/bash\n#Pipeline generated by ANGSDgui\n#"+ Json[JsonAnalysis].Name + "\n\n";
-
-                // update pipeline
-                rewriteCode();
-
-                break;
-            }
-        }
-    }
-    else {
-
-            CodeHeader = "#!bin/bash\n#Pipeline generated by ANGSDgui\n";
-            rewriteCode();
-            
-    }
-
-    
-
-    /*
-    //  if no analysis selected
-    if (AnalysisNo == 0){
-
-        CodeHeader = "#!bin/bash\n#Pipeline generated by ANGSDgui\n";
-        rewriteCode();
-
-    // site frequency spectrum
-    }else if (AnalysisName == Json.SFS[0].AnalysisName){
         addStep("step-2");
 
-        // add analysis specific input files
-        InfileTypes[Json.SFS[0].Infile[0]] = Json.SFS[0].Infile[1];
-        //InfileTypes["saf.idx"] = "saf"    
+        var InfileTypes = Json[AnalysisName].Infiles;
 
         // create input file type select
         var InfileType = document.createElement('select');  
@@ -649,15 +560,14 @@ var getAnalysis = function(){
         createLabel(InfileTypeLabel, InfileType.id, " Input file type ", "Required");
         var InfileTypePop = document.createElement('a');
         createPopover(
-                InfileTypePop, 
-                "Input File Type", 
-                `File type of input file. <br />
-                For more information, see <a href="http://www.popgen.dk/angsd/index.php/Input" target="_blank">angsd wiki input page</a>.
-                `
+            InfileTypePop, 
+            "Input File Type", 
+            `File type of input file. <br />
+            For more information, see <a href="http://www.popgen.dk/angsd/index.php/Input" target="_blank">angsd wiki input page</a>.
+            `
         );
-        addFormItem("step-2-div", InfileTypePop, InfileTypeLabel, InfileType);
-        FunctionList.push(getInfileType);
-     
+        addFormItem("step-2-div", InfileType, InfileTypeLabel, InfileTypePop);
+
         // create input file name input
         var InfileName = document.createElement('input');
         createInput(InfileName, "infileName", "Required");
@@ -665,58 +575,33 @@ var getAnalysis = function(){
         createLabel(InfileNameLabel, InfileName.id, " Input file name ", "Required");
         var InfileNamePop = document.createElement('a');
         createPopover(
-                InfileNamePop, 
-                "Input File Name", 
-                `Name of the input file to be used in the analysis. Including extensions if any.
-                Provide path to input file if you are not going to run the code in the same directory as the input file.
-                e.g. <code>/home/myuser/project/myfile.bamlist</code>`
+            InfileNamePop, 
+            "Input File Name", 
+            `Name of the input file to be used in the analysis. Including extensions if any.
+            Provide path to input file if you are not going to run the code in the same directory as the input file.
+            e.g. <code>/home/myuser/project/myfile.bamlist</code>`
         );
-        addFormItem("step-2-div", InfileNamePop, InfileNameLabel, InfileName);
+        addFormItem("step-2-div", InfileName, InfileNameLabel, InfileNamePop);
         FunctionList.push(getValue.bind(null, "", "infileName"));
 
         // enable new popovers
         enableNewPops();
 
         // add analysis name to pipeline header
-        CodeHeader = "#!bin/bash\n#Pipeline generated by ANGSDgui\n#"+ Json.SFS[0].AnalysisName + "\n\n";
+        CodeHeader = "#!bin/bash\n#Pipeline generated by ANGSDgui\n#"+ Json[AnalysisName].Name + "\n\n";
 
         // update pipeline
         rewriteCode();
-
-
-    // population branch statistics
-    } else if (AnalysisNo == 2){
-
-        // enable new popovers
-        enableNewPops();
-
-        // add analysis name to pipeline header
-        CodeHeader = "#!bin/bash\n#Pipeline generated by ANGSDgui\n#"+ Analysis.options[AnalysisNo].text + "\n\n";
-        //CodeHeader = "#!bin/bash\n#Pipeline generated by ANGSDgui\n#Population branch statistics\n\n";
-
-        // update pipeline
-        rewriteCode();
-
-
-    // calculate depth
-    } else if (AnalysisNo == 3){
-
-        // enable new popovers
-        enableNewPops();
-
-        // add analysis name to pipeline header
-        CodeHeader = "#!bin/bash\n#Pipeline generated by ANGSDgui\n#"+ Analysis.options[AnalysisNo].text + "\n\n";
-        //CodeHeader = "#!bin/bash\n#Pipeline generated by ANGSDgui\n#Depth calculation\n\n";
-
-        // update pipeline
-        rewriteCode();
-
     }
-    */
+    else {
 
+        CodeHeader = "#!bin/bash\n#Pipeline generated by ANGSDgui\n";
+        rewriteCode();
+            
+    }
 }
 
-
+    
 // add dynamic step
 var addStep = function(StepId){
 
