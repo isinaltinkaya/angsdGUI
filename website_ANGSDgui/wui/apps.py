ce58cf4e97c32d753ad173c21d41d582bf20ece3
isinaltinkaya <isinaltinkaya@gmail.com>
2020-06-20 16:11:10 +0300

    init github page

--- website_ANGSDgui/wui/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class WuiConfig(AppConfig):
-    name = 'wui'
--- website_ANGSDgui/wui/forms.py
+++
@@ -1,4 +0,0 @@
-from django.core import validators
-from django import forms
-from . import choices
-
